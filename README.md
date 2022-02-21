2f20d8486993f8c8c310ee244cafcbaf55a483ef
haobo <haobo@chipsea.com>
2022-02-21 11:52:55 +0800

    feat: add products of chipsea
    Signed-off-by: haobo <haobo@chipsea.com>

--- README.md
+++ README.md
@@ -1,39 +1,34 @@
 # vendor_chipsea
 
-#### 介绍
-{**以下是 Gitee 平台说明，您可以替换此简介**
-Gitee 是 OSCHINA 推出的基于 Git 的代码托管平台（同时支持 SVN）。专为开发者提供稳定、高效、安全的云端软件开发协作平台
-无论是个人、团队、或是企业，都能够用 Gitee 实现代码托管、项目管理、协作开发。企业项目请看 [https://gitee.com/enterprises](https://gitee.com/enterprises)}
+## 介绍
 
-#### 软件架构
-软件架构说明
+该仓库托管芯海科技开发的产品样例代码，主要包括TODO等特性产品的开发。
 
+## 软件架构
 
-#### 安装教程
+支持基于芯海CST85F01 SoC 的cst85_wblink单板进行开发的产品样例。
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+代码路径：
 
-#### 使用说明
+```
+vendor/chipsea/                    --- vendor_chipsea仓库路径
+└── wblink_demo                      --- 样例
+```
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+## 安装教程
 
-#### 参与贡献
+参考 [安装调试教程](https://gitee.com/openharmony-sig/device_soc_chipsea/blob/master/README.md)
 
-1.  Fork 本仓库
-2.  新建 Feat_xxx 分支
-3.  提交代码
-4.  新建 Pull Request
+## 使用说明
 
 
-#### 特技
+## 贡献
 
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+[如何参与](https://gitee.com/openharmony/docs/blob/HEAD/zh-cn/contribute/%E5%8F%82%E4%B8%8E%E8%B4%A1%E7%8C%AE.md)
+
+[Commit message规范](https://gitee.com/openharmony/device_qemu/wikis/Commit%20message%E8%A7%84%E8%8C%83?sort_id=4042860)
+
+## 相关仓
+
+[device/soc/chipsea](https://gitee.com/openharmony/device_soc_chipsea/blob/HEAD/README_zh.md)
+
---
+++ wblink_demo/app/wifi_demo/wifi_ap_demo.c
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2022 CHIPSEA Co., Ltd. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "wifi_device.h"
+#include "wifi_error_code.h"
+#include "wifi_hotspot.h"
+#include "rtos_al.h"
+#include "securec.h"
+#include "dbg.h"
+#include "ohos_init.h"
+
+#define AP_SSID "chipsea_test"
+#define AP_PSK "12345678"
+#define CHANNEL_NUM 7
+
+static void OnHotspotStaJoinHandler(StationInfo *info)
+{
+    if (info == NULL) {
+        dbg("HotspotStaJoin:info is null.\r\n");
+        return;
+    }
+    StationInfo stainfo[WIFI_MAX_STA_NUM] = {0};
+    StationInfo *sta_list_node = NULL;
+    unsigned int size = WIFI_MAX_STA_NUM;
+
+    // 获取当前热点sta列表
+    int error = GetStationList(stainfo, &size);
+    if (error != WIFI_SUCCESS) {
+        dbg("HotspotStaJoin:get list fail, error is %d.\r\n", error);
+        return;
+    }
+    sta_list_node = stainfo;
+    for (uint32_t i = 0; i < size; i++, sta_list_node++) {
+        unsigned char *mac = sta_list_node->macAddress;
+        dbg("HotspotSta[%u]: macAddress=%02X:%02X:%02X:%02X:%02X:%02X\r\n", i,
+               mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    }
+}
+
+// sta离开回调函数
+static void OnHotspotStaLeaveHandler(StationInfo *info)
+{
+    if (info == NULL) {
+        dbg("HotspotStaLeave:info is null.\r\n");
+        return;
+    }
+    unsigned char *mac = info->macAddress;
+    dbg("HotspotStaLeave: macAddress=%02X:%02X:%02X:%02X:%02X:%02X, reason=%d.\r\n",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5],
+           info->disconnectedReason);
+}
+
+// 热点状态变化回调函数
+static void OnHotspotStateChangedHandler(int state)
+{
+    dbg("HotspotStateChanged:state is %d.\r\n", state);
+    if (state == WIFI_HOTSPOT_ACTIVE) {
+        dbg("wifi hotspot active.\r\n");
+    } else {
+        dbg("wifi hotspot noactive.\r\n");
+    }
+}
+
+WifiEvent g_wifiEventHandler = {
+    .OnHotspotStaJoin = OnHotspotStaJoinHandler,
+    .OnHotspotStaLeave = OnHotspotStaLeaveHandler,
+    .OnHotspotStateChanged = OnHotspotStateChangedHandler,
+};
+
+static void WifiApEntry(void)
+{
+    WifiErrorCode error;
+    osDelay(2000);
+
+    //注册wifi事件的回调函数
+    error = RegisterWifiEvent(&g_wifiEventHandler);
+    if (error != WIFI_SUCCESS) {
+        dbg("RegisterWifiEvent failed, error = %d.\r\n", error);
+        return;
+    }
+    dbg("RegisterWifiEvent succeed!\r\n");
+
+    HotspotConfig config = {0};
+    strcpy_s(config.ssid, WIFI_MAX_SSID_LEN, AP_SSID);
+    strcpy_s(config.preSharedKey, WIFI_MAX_KEY_LEN, AP_PSK);
+    config.securityType = WIFI_SEC_TYPE_PSK;
+    config.band = HOTSPOT_BAND_TYPE_2G;
+    config.channelNum = CHANNEL_NUM;
+
+    // 设置热点配置信息
+    error = SetHotspotConfig(&config);
+    if (error != WIFI_SUCCESS) {
+        dbg("SetHotspotConfig failed, error = %d.\r\n", error);
+        return;
+    }
+    dbg("SetHotspotConfig succeed!\r\n");
+
+    // 打开热点
+    error = EnableHotspot();
+    if (error != WIFI_SUCCESS) {
+        dbg("EnableHotspot failed, error = %d.\r\n", error);
+        return;
+    }
+    dbg("EnableHotspot succeed!\r\n");
+
+    //检查热点是否已开启
+    if (IsHotspotActive() == WIFI_HOTSPOT_NOT_ACTIVE) {
+        dbg("Wifi Hotspot is not actived.\r\n");
+        return;
+    }
+    dbg("Wifi Hotspot is actived!\r\n");
+    while(1);
+}
+
+void WifiApDemo(void)
+{
+    dbg("[%s:%d]: %s\n", __FILE__, __LINE__, __func__);
+
+    if (rtos_task_create(WifiApEntry, "wifi ap demo task", 0, 0x500, NULL, RTOS_TASK_PRIORITY(1), NULL)) {
+        return -1;
+    }
+}
+
+SYS_RUN(WifiApDemo);
